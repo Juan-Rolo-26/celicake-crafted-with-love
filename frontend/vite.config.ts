9c341f1e917caf34433c92d702289d57ab30ebdc
Juan Pablo Rolo <juanpablorolo2007@gmail.com>
2026-01-22 17:46:23 +0000

    s

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/celicake-crafted-with-love/", // 👈 NOMBRE DEL REPO
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
